e8e997bac8f0d783289506b6df4c089926b7612b
dishj2024 <dishajain2358@gmail.com>
2025-01-13 14:17:24 +0530

    modified

--- sumof2no.py
+++ sumof2no.py
@@ -1,4 +1,9 @@
 # sum of two number (input by user )
+
 a= int(input("enter no. 1 : "))
 b= int(input("enter no. 2 : "))
 print("sum of no.1 and no2",a+b)
+
+# input is user for take value from user 
+
+#int is used to convert it to integer from string
