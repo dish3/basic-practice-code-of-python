b98ed08922826c4bc692cfcbdab8973439684a85
dishj2024 <dishajain2358@gmail.com>
2025-01-16 13:35:43 +0530

    if-if-else

--- condition_statements.py
+++ condition_statements.py
@@ -12,7 +12,7 @@ else:
 
 """
 #example 
-
+"""
 marks = int(input("enter marks : "))
 
 if(marks>=90):
@@ -23,3 +23,20 @@ elif(80> marks >= 70):
     print("C")
 else:
     print("D")
+
+"""
+# if-if-elif-else
+
+age= int(input("enter your age : "))
+
+license = input("yes or no : ")
+
+if(80> age >=18):
+    if(license== "yes"):
+        print("you can drive")
+    elif(license== "no"):
+        print("you can't drive")
+    else:
+        print("invalid input for license")
+else:
+    print("incorrect information filled")
