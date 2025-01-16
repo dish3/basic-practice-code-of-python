1d8e95ce449302216b750022cb627be934dc4848
dishj2024 <dishajain2358@gmail.com>
2025-01-16 12:14:42 +0530

    strings program with explaination

---
+++ strings_programs.py
@@ -0,0 +1,11 @@
+# to find length of string 
+"""
+ufn= (input("enter your first name "))
+print("length of user first name is : ", len(ufn)) <---- used for length
+print("your name is : ", ufn) """
+
+#to find occurrence of "$" in string
+"""
+count1 = input("enter a string with dollar: ")
+print(" so the occurrence of '$' in string is :", count1.count("$"))<--- to find count
+"""
