3bd8f2665e350d90f5e7350673044f88d26ef43b
dishj2024 <dishajain2358@gmail.com>
2025-02-04 16:31:59 +0530

    loops completed

--- loops_while_for.py
+++ loops_while_for.py
@@ -29,6 +29,7 @@ while ind < len(list1):
     ind += 1
 """
 #search for a number x in this tuple using loop
+"""
 list1=(1,4,9,16,25,36,49,64,81,100)
 x=int(input("enter the no: "))
 i=0
@@ -42,4 +43,40 @@ while i< len(list1):
 
 if not found:
         print("not found")
-   
+   """
+#print the elements of the following list using a loop 
+"""
+num=[1,4,9,16,25,36,49,64,81,100] 
+el=int(input("enter a no:"))
+id=0
+for x in num:
+    if(x == el):
+        print(f"found at {id}")
+    id+=1
+    print(x)
+
+"""    
+#using for & range()
+#print numbers from 1 to 100
+"""
+for i in range(1,101):
+    print(i)
+"""
+#WAP to find sum of first n numbers (using while)
+"""
+n=int(input("enter a no: "))
+sum=0
+for i in range(1,n+1):
+    sum+=i
+    print(f"Sum of first {n} numbers is: {sum}")
+    """
+#WAP to find factorial of first n number 
+"""
+n=int(input("enter a no:"))
+fact =1
+i=1
+while i<=n:
+    fact*=i
+    i+=1
+print("factorial= ",fact)
+"""
